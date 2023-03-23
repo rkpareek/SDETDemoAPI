0eb195b5cb494d870601d2d085068a8a54536d92
rkpareek <rkpareek002@gmail.com>
2023-03-23 20:57:00 +0530

    Added Password cases

---
+++ Test/Invalid PasswordCase.js
@@ -0,0 +1,29 @@
+/**
+ * This test suite includes test cases of Login API
+ */
+let supertest = require('supertest');
+let server = supertest.agent('https://reqres.in')
+let expect = require('chai').expect;
+
+describe('Login API Test Suite', () => {
+
+    //this is invalid password case
+
+    it('it should login when pass valid email and password', () => {
+        server
+            .post('/api/login')
+            .send(
+                {
+                    "email": "eve.holt@reqres.in",
+                    "password": "cityslicka"
+                }
+            )
+            .end(function (err, res) {
+                if (!err) {
+                    expect(res.statusCode).to.equal(200)
+                    expect(res.body).to.have.property('token')
+                }
+                else throw err;
+            })
+    })
+});
